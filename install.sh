4240d06cafe7bdab953ea103472dead204583fe8
gгёzы <69521267+undrcrxwn@users.noreply.github.com>
2022-11-11 14:37:27 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -305,8 +305,6 @@ check_run_command_as_root() {
   # Allow Azure Pipelines/GitHub Actions/Docker/Concourse/Kubernetes to do everything as root (as it's normal there)
   [[ -f /.dockerenv ]] && return
   [[ -f /proc/1/cgroup ]] && grep -E "azpl_job|actions_job|docker|garden|kubepods" -q /proc/1/cgroup && return
-
-  abort "Don't run this as root!"
 }
 
 should_install_command_line_tools() {
